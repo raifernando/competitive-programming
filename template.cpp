3c99445dc74a150fd7e5f610302472b04bb62c4f
raí fernando <raifernandodalpra@gmail.com>
2025-03-02 18:14:56 -0300

    template.cpp

---
+++ template.cpp
@@ -0,0 +1,20 @@
+// Problema: 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sp ' '
+#define INF 1000000007
+#define MAX 100
+
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+     
+
+    return 0;
+}
